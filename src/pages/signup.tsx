bfd17f54e397ce1f01b4712b5151ab8c89851a68
Uurtsaikh <uuree.i4uuree@gmail.com>
2024-03-31 23:58:15 +0800

    console

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,39 +1,88 @@
-import * as React from 'react';
+
+import React, {useState} from "react";
 import Checkbox from '@mui/material/Checkbox';
 
-import { FormControl, IconButton, OutlinedInput, Stack, TextField, Typography } from "@mui/material";
+import { Button, FormControl, IconButton, OutlinedInput, Stack, TextField, Typography } from "@mui/material";
 import { Visibility, VisibilityOff } from '@mui/icons-material';
 const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 const Home = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [address, setAddress] = useState("")
+  const [name, setName] = useState("")
+  const [emailError, setEmailError] = useState(false)
+  const [passwordError, setPasswordError] = useState(false)
+  const [nameError, setNameError] = useState(false)
+  const [addressError, setAddressError] = useState(false)
+
+
   const [showPassword, setShowPassword] = React.useState(false);
   const handleClickShowPassword = () => setShowPassword((show) => !show);
 const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
   };
+  const handleSubmit = (event:any) => {
+    event.preventDefault()
 
+    setEmailError(false)
+    setPasswordError(false)
+    setNameError(false)
+    setAddressError(false)
+
+    if (email == '') {
+        setEmailError(true)
+    }
+    if (password == '') {
+        setPasswordError(true)
+    }
+    if (address == '') {
+      setPasswordError(true)
+  }
+  if (name == '') {
+    setPasswordError(true)
+}
+
+    if (email && password && name && address ) {
+      
+      const data = {
+        email:email,
+        password:password,
+        name:name,
+        address:address,
+       }
+       console.log(data)
+    }
+    
+}
 
   return (
     <>
-      <main>
+
         <Stack>
+
+        <React.Fragment>
+        <form autoComplete="off" onSubmit={handleSubmit}>
           <Stack width={"448px"} padding={4} gap={6} margin={"auto"} marginTop={"74px"} borderRadius={2} marginBottom={"107px"}>
             <Typography fontSize={"28px"} fontWeight={"700"} margin={"auto"}>Бүртгүүлэх</Typography>
             <Stack gap={2}>
               <Stack gap={"4px"}>
                 <Typography>Нэр</Typography>
-               <TextField  id="outlined-basic" placeholder="Нэрээ оруулна уу" variant="outlined" sx={{backgroundColor:'#ECEDF0'}}/>
+               <TextField    onChange={e => setName(e.target.value)}
+                  required id="outlined-basic" type="name" placeholder="Нэрээ оруулна уу" variant="outlined" sx={{backgroundColor:'#ECEDF0'}}/>
                 
               </Stack>
               <Stack gap={"4px"}>
                 <Typography>И-мэйл</Typography>
                 
-                <TextField  id="outlined-basic" placeholder="И-мэйл хаягаа оруулна уу" variant="outlined" sx={{backgroundColor:'#ECEDF0'}} />
+                <TextField    onChange={e => setEmail(e.target.value)}
+                  required id="outlined" type="email" placeholder="И-мэйл хаягаа оруулна уу" variant="outlined" sx={{backgroundColor:'#ECEDF0'}} />
                 
               </Stack>
               <Stack gap={"4px"}>
                 <Typography>Хаяг</Typography>
                 
-                <TextField  id="outlined-basic" placeholder="Та хаягаа оруулна уу" variant="outlined" sx={{backgroundColor:'#ECEDF0'}}/>
+                <TextField    onChange={e => setAddress(e.target.value)}
+                  required id="outline" type="address" placeholder="Та хаягаа оруулна уу" variant="outlined" sx={{backgroundColor:'#ECEDF0'}}/>
                 
               </Stack>
               <Stack gap={"4px"}>
@@ -42,7 +91,7 @@ const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => 
                 <FormControl sx={{ m: 0, width: '100%', backgroundColor:'#ECEDF0' }} variant="outlined">
           
           <OutlinedInput
-            id="outlined-adornment-password"
+            id="outlined-adornment"
             type={showPassword ? 'text' : 'password'}
             endAdornment={
               
@@ -67,6 +116,8 @@ const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => 
                 <FormControl sx={{ m: 0, width: '100%', backgroundColor:'#ECEDF0' }} variant="outlined">
           
           <OutlinedInput
+             onChange={e => setPassword(e.target.value)}
+             required
             id="outlined-adornment-password"
             type={showPassword ? 'text' : 'password'}
             endAdornment={
@@ -93,15 +144,27 @@ const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => 
                  <Typography mt={1}>Үйлчилгээний нөхцөл зөвшөөрөх</Typography>
                </Stack>
              
-               <TextField  id="outlined-basic"  variant="outlined" sx={{backgroundColor:'#ECEDF0'}}
-                 type='submit'
-                 value={"Бүртгүүлэх"}
-                 name='submit'
-               />
+               <Button
+
+type="submit"
+  sx={{
+    px: "16px",
+    py: "8px",
+    borderRadius: "8px",
+    backgroundColor: "#18BA51",
+    color: "white",
+    fontSize: "16px",
+    border: "1px solid #18BA51",
+  }}
+>
+Бүртгүүлэх
+</Button>
             </Stack>
          </Stack>
+         </form>
+         </React.Fragment>
         </Stack>
-      </main>
+    
     </>
   );
 };

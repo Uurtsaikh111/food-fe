170d38c832432b6170983e87f872194290ac93d2
Үүрцайх Норовсамбуу <23lp8969@nest.edu.mn>
2024-04-08 13:27:06 +0800

    class

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,17 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/layout/Layout";
 import { CustomThemeProvider } from "@/utils/CustomTheme";
-import { CustomContextProvider } from "@/context/FoodsCard"; 
+import { CustomContextProvider } from "@/context/FoodsCard";
 const App = ({ Component, pageProps }: AppProps) => {
-  return     <CustomContextProvider><CustomThemeProvider><Layout><Component {...pageProps} /></Layout></CustomThemeProvider>  </CustomContextProvider>;
+  return (
+    <CustomContextProvider>
+      <CustomThemeProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </CustomThemeProvider>{" "}
+    </CustomContextProvider>
+  );
 };
 
 export default App;
